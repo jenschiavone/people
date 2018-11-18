e29cf1fcf6feaf12662047b08e0630cb5378f239
Jennifer Schiavone <jennifer.schiavone@manheim.com>
2018-11-18 00:36:52 -0500

    Get email character counts

--- src/app/ui/people-emails/people-emails.component.ts
+++ src/app/ui/people-emails/people-emails.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Person } from '../people/person';
+import { PeopleService } from '../people/people.service';
 
 @Component({
   selector: 'app-people-emails',
@@ -6,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PeopleEmailsComponent implements OnInit {
 
-  constructor() { }
+  people: Person[];
+  occurrences: object;
+
+  getEmailCharacterCounts(people) {
+    this.occurrences = this.peopleService.getEmailCharacterCounts(people);
+  }
+
+  constructor(private peopleService: PeopleService) { }
 
   ngOnInit() {
+    this.peopleService.getPeople().subscribe(people => this.people = people)
   }
 
 }
--- src/app/ui/people/people.service.ts
+++ src/app/ui/people/people.service.ts
@@ -18,4 +18,30 @@ export class PeopleService {
     return this.http.get(this.peopleUrl)
                     .map((response: Response) => <Person[]>response.json())
   }
+
+  getEmailCharacterCounts(people) {
+    var occurrences = {};
+    let emails = [];
+    for (let person of people) {
+      emails.push(person.email_address);
+    }
+    const allChars = (emails.join('').trim()).replace(/\s/g, '').split('');
+    const unique = (value, index, self) => {
+      return self.indexOf(value) === index;
+    }
+    const uniqueChars = allChars.filter(unique);
+
+    for (let char of allChars) {
+      for (let uniqueChar of uniqueChars) {
+        if (uniqueChar === char) {
+          if (!occurrences.hasOwnProperty(uniqueChar)) {
+            occurrences[uniqueChar] = 1;
+          } else {
+            occurrences[uniqueChar] = occurrences[uniqueChar] + 1;
+          }         
+        } 
+      }
+    }
+    return occurrences;
+  }
 }
